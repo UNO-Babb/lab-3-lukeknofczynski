223558926eee0dde02b10c66b0b03ba282774709
lukeknofczynski <lknofczynski@unomaha.edu>
2025-02-05 16:15:32 +0000

    All Done

--- RPS.py
+++ RPS.py
@@ -1,22 +1,56 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Luke Knofczynski
+#Date: 2/5/25
+#Assignment: 
 import random
 
 def main():
   wins = 0
   ties = 0
   losses = 0
-  #Create a loop that continues as long as the user wants to play.
-  #User can play as many games as they wish.
+  play = "Y"
+  while play == "Y":
+    
+    player = input("Enter Rock(R), Paper(P), or Scissors(S): ")
+    
+    computer = random.choice(["R", "P", "S"])
+    
+    print("Computer choose:", computer)
+    if player == "R":
+      if computer == "R":
+        print("Tie!")
+        ties = ties + 1
+      if computer == "S":
+        print("You Win!")
+        wins = wins + 1
+      if computer =="P":
+        print("You Lose!")
+        losses = losses +1
+    elif player == "P":
+      if computer == "P":
+        print("Tie!")
+        ties = ties + 1
+      if computer == "R":
+        print("You Win!")
+        wins = wins + 1
+      if computer =="S":
+        print("You Lose!")
+        losses = losses +1
+    elif player == "S":
+      if computer == "S":
+        print("Tie!")
+        ties = ties + 1
+      if computer == "P":
+        print("You Win!")
+        wins = wins + 1
+      if computer =="R":
+        print("You Lose!")
+        losses = losses +1
+    else:
+      print("Invaild Input")
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
-
-  #In the end, print the stats
+    play = input("play again? (Y/N): ")
+  print("Thanks for Playing")
   print("Wins \t Ties \t Losses")
   print("---- \t ---- \t ------")
   print(wins, "\t", ties , "\t", losses)
--- TempConvert.py
+++ TempConvert.py
@@ -1,6 +1,6 @@
 #TempConvert.py
-#Name:
-#Date:
+#Name: Luke Knofczynski
+#Date: 2/5/25
 #Assignment:
 
 
@@ -8,10 +8,11 @@ def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
+  tempF = input("Enter a temperature in Fahrenheit: ")
+  tempF =int(tempF)
 
-  tempC = tempF / 2
-
+  tempC = (tempF-32)*5/9
+  tempC = round(tempC, 2)
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
